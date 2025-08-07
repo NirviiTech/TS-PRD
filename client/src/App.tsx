318c324f1dde94296636df177dabd36856700c22
NirviiTech <mohitpen123@gmail.com>
2025-08-07 16:45:41 +0530

    Changes the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-pink-100 text-5xl text-amber-300'>App</div>
+    <div className='text-red-500'>App</div>
   )
 }
 
